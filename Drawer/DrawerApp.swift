9d63f6f2d2ff8674926419d01232129011d37cc4
John Xavier <johnxavier.sics@gmail.com>
2021-12-11 14:01:43 +0530

    Initial Commit

---
+++ Drawer/DrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerApp.swift
+//  Drawer
+//
+//  Created by John Srishti on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
